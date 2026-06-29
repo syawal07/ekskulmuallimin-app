0ac82c0276f00c3fb3289473317a45548cffcf40
syawalsss <syawal_ojt@solvera.id>
2026-06-29 14:04:23 +0700

    update commit new fitur nextconfig

--- muallimin-ekskul-fe/next.config.ts
+++ muallimin-ekskul-fe/next.config.ts
@@ -7,7 +7,7 @@ const nextConfig: NextConfig = {
       { protocol: 'https', hostname: 'plus.unsplash.com' },
       { protocol: 'http', hostname: '127.0.0.1' },
       { protocol: 'http', hostname: 'localhost' },
-      { protocol: 'https', hostname: 'ekskul.muallimin.sch.id' },
+      { protocol: 'https', hostname: 'kesiswaan.muallimin.sch.id' },
       { protocol: 'https', hostname: 'api-ekskul.muallimin.sch.id' }
     ]
   },
